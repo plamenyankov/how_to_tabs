28006c3486cb4fcd763aeefd2b902477f0b7f41f
Plamen Yankov <p.s.yankov@abv.bg>
2016-05-12 14:43:25 +0300

    adding semver

--- jakefile.js
+++ jakefile.js
@@ -1,7 +1,20 @@
 (function(){
 'use strict';
-    console.log("\n\nHello world");
-    task('default',function(){
-       console.log('jake ')
+    var semver = require('semver');
+    desc("Default jake task");
+    task("default",["version"],function(){
+        console.log("\n\nBUILD OK");
     });
+    desc("Checking Node version");
+    task("version",function(){
+        console.log("Cheking Node version");
+        var expectedVersion = require('./package.json').engines.node;
+
+        var actualVersion = process.version;
+       if(semver.neq(actualVersion,expectedVersion)){
+           fail("Incorrect Node version: expected "+expectedVersion+ " but was "+ actualVersion);
+       }
+    });
+
+
 }());
