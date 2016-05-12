c62c06f61337b189b530e606eb3e9cd97befa881
Plamen Yankov <p.s.yankov@abv.bg>
2016-05-12 17:08:43 +0300

    adding jshint to jakefile

--- jakefile.js
+++ jakefile.js
@@ -1,20 +1,22 @@
-(function(){
-'use strict';
+(function () {
+    'use strict';
     var semver = require('semver');
     desc("Default jake task");
-    task("default",["version"],function(){
+    task("default", ["version","lint"], function () {
         console.log("\n\nBUILD OK");
     });
     desc("Checking Node version");
-    task("version",function(){
+    task("version", function () {
         console.log("Cheking Node version");
         var expectedVersion = require('./package.json').engines.node;
 
         var actualVersion = process.version;
-       if(semver.neq(actualVersion,expectedVersion)){
-           fail("Incorrect Node version: expected "+expectedVersion+ " but was "+ actualVersion);
-       }
+        if (semver.neq(actualVersion, expectedVersion)) {
+            fail("Incorrect Node version: expected " + expectedVersion + " but was " + actualVersion);
+        }
     });
-
+    task("lint", function () {
+        jake.exec("node node_modules/jshint/bin/jshint jakefile.js",{interactive:true});
+    },{async:true});
 
 }());
