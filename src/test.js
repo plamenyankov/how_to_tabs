bf98905b95ac81e8eb86c2d2a75e92e2ad9cecc2
Plamen Yankov <p.s.yankov@abv.bg>
2016-05-13 18:47:39 +0300

    adding mocha

--- src/test.js
+++ src/test.js
@@ -2,7 +2,7 @@
 (function () {
     "use strict";
     var assert = require("chai").assert;
-    assert.equal(add(1,2),3);
+    assert.equal(add(0.1,0.2),0.30000000000000004);
     function add(a, b) {
         return a + b;
     }
