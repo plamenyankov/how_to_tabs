d94e7f78325535c5e1e044ab19a01e7ce4b6ed5e
Plamen Yankov <p.s.yankov@abv.bg>
2016-05-14 16:09:00 +0300

    adding expected browsers

--- jakefile.js
+++ jakefile.js
@@ -3,13 +3,17 @@
     'use strict';
     var semver = require('semver');
     var jshint = require('simplebuild-jshint');
+    var karma = require('simplebuild-karma');
+    var KARMA_CONFIG = "karma.conf.js";
     //*** General purpose tasks
     desc("Start the karma server");
     task("karma", function () {
-        console.log("karma");
-    });
+        karma.start({
+        configFile:KARMA_CONFIG
+        },complete,fail);
+    },{async:true});
     desc("Default jake task");
-    task("default", ["version", "lint"], function () {
+    task("default", ["version", "lint","test"], function () {
         console.log("\n\nBUILD OK");
     });
     desc("Run http server");
@@ -29,6 +33,7 @@
             fail("Incorrect Node version: expected " + expectedVersion + " but was " + actualVersion);
         }
     });
+    desc("Linting");
     task("lint", function () {
         process.stdout.write("Javascript linting ");
         jshint.checkFiles({
@@ -38,6 +43,15 @@
         }, complete, fail);
         //jake.exec("node node_modules/jshint/bin/jshint jakefile.js",{interactive:true},complete);
     }, {async: true});
+    desc("Run test");
+    console.log("Running tests:");
+    task("test", function () {
+        karma.run({
+            configFile:KARMA_CONFIG,
+            expectedBrowsers:expectedBrowsers(),
+            strict:!process.env.loose
+        },complete,fail);
+    },{async:true});
     function lintOpt() {
         return {
             bitwise: true,
@@ -67,5 +81,11 @@
             beforeEach: false,
             afterEach: false
         };
+    }
+    function expectedBrowsers(){
+        return [
+            "Chrome 50.0.2661 (Windows 10 0.0.0)",
+            "Firefox 46.0.0 (Windows 10 0.0.0)"
+        ];
     }
 }());
