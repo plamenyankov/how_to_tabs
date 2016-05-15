ec04f04762e9a0aa9a4e0fd2f8359d1d291fc066
Plamen Yankov <p.s.yankov@abv.bg>
2016-05-15 14:37:52 +0300

    Install shelljs

--- jakefile.js
+++ jakefile.js
@@ -4,7 +4,9 @@
     var semver = require('semver');
     var jshint = require('simplebuild-jshint');
     var karma = require('simplebuild-karma');
+    var shell = require('shelljs');
     var KARMA_CONFIG = "karma.conf.js";
+    var DIST_DIR = "generated/dist";
     //*** General purpose tasks
     desc("Start the karma server");
     task("karma", function () {
@@ -17,8 +19,8 @@
         console.log("\n\nBUILD OK");
     });
     desc("Run http server");
-    task("run", function () {
-        jake.exec("node node_modules/http-server/bin/http-server src", {interactive: true, async: true}, complete);
+    task("run",["build"], function () {
+        jake.exec("node node_modules/http-server/bin/http-server "+DIST_DIR, {interactive: true, async: true}, complete);
     });
 
 
@@ -44,7 +46,6 @@
         //jake.exec("node node_modules/jshint/bin/jshint jakefile.js",{interactive:true},complete);
     }, {async: true});
     desc("Run test");
-    console.log("Running tests:");
     task("test", function () {
         karma.run({
             configFile:KARMA_CONFIG,
@@ -52,6 +53,17 @@
             strict:!process.env.loose
         },complete,fail);
     },{async:true});
+    desc("Build distribution directory");
+    task("build",[DIST_DIR],function(){
+       console.log("Build");
+    });
+    desc("Erase all build files");
+    task("clean",function(){
+       console.log("Erase Build files");
+        shell.rm("-rf","generated");
+    });
+    directory(DIST_DIR);
+
     function lintOpt() {
         return {
             bitwise: true,
