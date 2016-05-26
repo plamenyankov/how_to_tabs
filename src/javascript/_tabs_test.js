6055f23bbc5ab372bfa2aa875fd0da9218729825
Plamen Yankov <p.s.yankov@abv.bg>
2016-05-26 15:36:45 +0300

    style the default tab

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -13,16 +13,21 @@
             removeElement(container);
         });
 
-        it("hides all content elements except the default upon initialization",function(){
+        it("hides all content elements except the default upon initialization", function () {
+            var tab1 = addElement("div");
+            var defaultTab = addElement("div");
+            var tab3 = addElement("div");
             var element1 = addElement("div");
             var defaultElement = addElement("div");
             var element3 = addElement("div");
 
             tabs.initialize({
-                content:[element1,defaultElement, element3 ],
-                default:defaultElement,
-                contentHideClass:"hideClass"
-        });
+                tabs:[tab1,defaultTab,tab3],
+                content: [element1, defaultElement, element3],
+                default: defaultElement,
+                activeTabClass:"activeTab",
+                contentHideClass: "hideClass"
+            });
             assert.equal(getClasses(element1), "hideClass", "element 1 should be hidden");
             assert.equal(getClasses(defaultElement), "", "default element should not be hidden");
             assert.equal(getClasses(element3), "hideClass", "element 3 should be hidden");
@@ -31,20 +36,37 @@
         it("preserves existing classes when hiding a content element", function () {
             var hiddenElement = addElement("div");
             var defaultElement = addElement("div");
+            var tab1 = addElement("div");
+            var defaultTab = addElement("div");
 
             hiddenElement.setAttribute("class", "existingClass");
             tabs.initialize({
-                content:[hiddenElement,defaultElement],
-                default:defaultElement,
-                contentHideClass:"someClass"
+                tabs:[tab1,defaultTab],
+                content: [hiddenElement, defaultElement],
+                default: defaultElement,
+                activeTabClass:"activeTab",
+                contentHideClass: "someClass"
             });
             assert.equal(getClasses(hiddenElement), "existingClass someClass");
 
         });
+        it("styles the default tab with a class", function () {
+            var defaultTab = addElement("div");
+            var defaultElement = addElement("div");
+            tabs.initialize({
+                tabs:[ defaultTab ],
+                content: [defaultElement],
+                default: defaultElement,
+                activeTabClass:"activeTab",
+                contentHideClass: "ignored"
+            });
+            assert.equal(getClasses(defaultTab), "activeTab");
 
-        function removeElement(el){
+        });
+        function removeElement(el) {
             el.parentNode.removeChild(el);
         }
+
         function addElement(el) {
             var element = document.createElement(el);
             container.appendChild(element);
--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -3,10 +3,14 @@
     var classList = require("../vendors/classList.js");
     classList.shim();
     exports.initialize = function initialize(options){
+        var tabs = options.tabs;
         var content = options.content;
         var className = options.contentHideClass;
         var defaultElement = options.default;
-        
+        var activeTabClass = options.activeTabClass;
+
+        if(tabs === undefined) throw new Error("Expected options.tabs");
+        if(activeTabClass === undefined) throw new Error("Expected options.activeTabClass");
         if(content === undefined) throw new Error("Expected options.content");
         if(className === undefined) throw new Error("Expected options.contentHideClass");
         if(defaultElement === undefined) throw new Error("Expected options.default");
@@ -15,6 +19,7 @@
            element.classList.add(className);
         });
         defaultElement.classList.remove(className);
+        tabs[0].classList.add(activeTabClass);
     };
 
 }());
